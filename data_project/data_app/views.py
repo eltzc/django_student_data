a4e6d4afde09f3ab814e3b09e9695b716c283353
eltzc <vasechkin.lelya@inbox.ru>
2025-10-02 23:19:20 +0500

    Step 5: Implemented display of student data list

--- data_project/data_app/views.py
+++ data_project/data_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import StudentDataForm
+from .models import StudentData  # <- Импортируйте модель StudentData
 
 def student_data_create(request):
     if request.method == 'POST':
         form = StudentDataForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('student_data_list')  # После сохранения перенаправляем на список
+            return redirect('student_data_list')
     else:
         form = StudentDataForm()
     return render(request, 'data_app/student_data_form.html', {'form': form})
 
 def student_data_list(request):
-    # Позже добавим логику для отображения списка
-    return render(request, 'data_app/student_data_list.html', {})
+    students = StudentData.objects.all()  # <- Получите все записи из базы данных
+    return render(request, 'data_app/student_data_list.html', {'students': students})  # <- Передайте их в шаблон
