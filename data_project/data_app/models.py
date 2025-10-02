d6cdec2a241343115e775ef501f710a1ec86cd1c
eltzc <vasechkin.lelya@inbox.ru>
2025-10-02 23:09:36 +0500

    Step 2: Defined StudentData model and applied migrations

--- data_project/data_app/models.py
+++ data_project/data_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class StudentData(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    grade = models.DecimalField(max_digits=5, decimal_places=2)  # Например, 100.00
+    date_received = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.subject}"
